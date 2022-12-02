7379df8418727bea06511b916511108fe5b2ff04
RRooby Joseph <roobyst03@gmail.com>
2022-12-01 23:10:31 -0400

    deleted purchases

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -64,3 +64,48 @@ const ProductDetail = () => {
 };
 
 export default ProductDetail;
+
+
+/*
+import React, { useEffect } from 'react';
+import { ListGroup } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
+import { getPurchasesThunk } from '../store/slices/purchases.slice';
+
+const Purchases = () => {
+
+    const dispatch = useDispatch();
+
+    const navigate = useNavigate();
+
+    const purchases = useSelector(state => state.purchases);
+
+    useEffect(() => {
+        dispatch(getPurchasesThunk());
+    }, []);
+
+    return (
+        <div>
+            <h2>pruchases hello</h2>
+            <ListGroup>
+                {purchases.map(purchase => (
+                    <ul >
+                        {purchase.cart.products.map(product => (
+                            <li >
+                                <Link to={`/products/${product?.id}`}>
+                                    {product.title}
+                                    {product.price}
+                                    {product.createdAt}
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                ))}
+            </ListGroup>
+        </div>
+    );
+};
+
+export default Purchases;
+*/
